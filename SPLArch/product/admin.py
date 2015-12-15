33aa691ba6665e1789cef37f1f42b5acccb071de
randler <randlersi@gmail.com>
2015-12-15 09:30:12 -0300

    Alteracoes em Product

--- SPLArch/product/admin.py
+++ SPLArch/product/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from SPLArch.product.models import Product
 
 
-admin.site.register(Product)
+class productAdmin(admin.ModelAdmin):
+    fieldsets = [
+                 ('Products', {'fields': ['name','description'], 'classes': ['collapse']})
+                 ]
+
+admin.site.register(Product, productAdmin)
--- SPLArch/product/models.py
+++ SPLArch/product/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
