67120dbbfff14223e0e558ac061487c1d77154e8
italo-miranda <italomirandadenovais@gmail.com>
2015-12-17 01:30:24 -0200

    Classes UseCase, MainSteps e AlternativeSteps

--- SPLArch/product/models.py
+++ SPLArch/product/models.py
@@ -15,3 +15,27 @@ class Project(models.Model):
 
     def __unicode__(self):
         return self.name
+
+class UseCase(models.Model):
+   description = models.TextField(blank=True)
+   precondition = models.TextField(blank=True)
+   title = models.CharField(max_length=200)
+
+   def __unicode__(self):
+        return self.name
+
+class MainSteps(models.Model):
+    use_case = models.ForeignKey(UseCase)
+    system_response = models.TextField(blank=True)
+    user_action = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+class AlternativeSteps(models.Model):
+    use_case = models.ForeignKey(UseCase)
+    system_response = models.TextField(blank=True)
+    user_action = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
